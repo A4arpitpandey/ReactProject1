ad07b217f87b7276c5097c0d40607d346a53fe6a
A4arpitpandey <arpitpandey1100@gmail.com>
2025-01-25 14:48:03 +0530

    firts project using raect vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "Chai"
+
+
+
+function App() {
+
+
+  return (<Chai></Chai>
+    
+  )
+    
+}
+
+export default App
---
+++ src/Chai.jsx
@@ -0,0 +1,3 @@
+function Chai(){
+  return <h3>Arpit Please have a tea</h3>
+}
